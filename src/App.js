6543960fbe4dcc225c9fac384487fcd8b889bf18
Steeb <steeb@Nicoles-MacBook-Air.local>
2025-10-28 20:02:31 -0700

    Intial save point for github repo intialization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { getAllSpells } from "./api";
+import SpellCard from "./spellCard";
+import "./App.css";
+import "./style.css";
 
-function App() {
+// Your main App component
+function AppContent() {
+  const [spells, setSpells] = useState([]);
+
+  useEffect(() => {
+    const savedSpells = localStorage.getItem("spells");
+    if (savedSpells){
+      setSpells(JSON.parse(savedSpells));
+    } else {
+      getAllSpells().then((spells) => {
+        setSpells(spells);
+        localStorage.setItem("spells", JSON.stringify(spells));
+      });
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {spells.length === 0 && <span className="loading">Loading...</span>}
+      <div className="spell-list">
+        {spells.map((spell) => (
+          <SpellCard key={spell.index} spell={spell} />
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+// Root component with providers
+export default function App() {
+  return (
+    <ChakraProvider>
+      <AppContent />
+    </ChakraProvider>
+  );
+}
